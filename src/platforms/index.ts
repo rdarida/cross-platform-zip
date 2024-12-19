325b096e58752ee242531ae69fe02cdb72575ee9
Róbert Darida <rdarida@users.noreply.github.com>
2024-12-19 20:19:23 +0100

    feat: creat windows platform

---
+++ src/platforms/index.ts
@@ -0,0 +1,1 @@
+export * as windows from './windows';
--- src/platforms/windows.ts
+++ src/platforms/windows.ts
@@ -1,8 +1,7 @@
-import { windows } from './platforms';
-
 /**
  * Creates a compressed archive, or zipped file,
  * from specified files and folders.
+ * This is a wrapper function for Compress-Archive.
  *
  * @param path Specifies the path or paths to the files
  *             to add to the archive zipped file.
@@ -10,41 +9,20 @@ import { windows } from './platforms';
  * @param dest Specifies the path to the archive output file.
  */
 export function zipSync(path: string | string[], dest: string): void {
-  switch (process.platform) {
-    case 'win32': {
-      windows.zipSync(path, dest);
-      break;
-    }
-
-    case 'darwin':
-    case 'linux': {
-      break;
-    }
-
-    default:
-      break;
-  }
+  console.log('windows.zipSync');
+  console.log(path);
+  console.log(dest);
 }
 
 /**
  * Extracts files from a specified archive (zipped) file.
+ * This is a wrapper function for Expand-Archive.
  *
  * @param path Specifies the path to the archive file.
  * @param dest Specifies the path to the output folder.
  */
-export function unzipSync(path: string, dest: string): void {
-  switch (process.platform) {
-    case 'win32': {
-      windows.unzipSync(path, dest);
-      break;
-    }
-
-    case 'darwin':
-    case 'linux': {
-      break;
-    }
-
-    default:
-      break;
-  }
+export function unzipSync(path: string | string[], dest: string): void {
+  console.log('windows.unzipSync');
+  console.log(path);
+  console.log(dest);
 }
