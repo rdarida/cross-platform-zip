c4c67d39fbe71005f476f95a52a16f09f16a31e1
Róbert Darida <rdarida@users.noreply.github.com>
2024-12-20 02:29:56 +0100

    fix: use cwd in unix.zipSync

--- src/platforms/unix.ts
+++ src/platforms/unix.ts
@@ -19,8 +19,9 @@ export function zipSync(path: string | string[], dest: string): void {
 
   execFileSync(
     'zip',
-    [dest, path[0]],
+    [dest, basename(path[0])],
     {
+      cwd: dirname(path[0]),
       maxBuffer: Infinity,
       windowsHide: true,
     }
