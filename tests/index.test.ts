6f3d4d80a1fec4f591b2293ea820d1ce0ef9f0d2
Róbert Darida <rdarida@users.noreply.github.com>
2024-12-21 11:28:52 +0100

    test: cover the single 'path' argument of zipSync

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -6,15 +6,18 @@ import { zipSync, unzipSync } from '../src/index';
 
 const TXT1 = 'test data 1.txt';
 const TXT2 = 'test data 2.txt';
-const ZIP = 'test data.zip';
+const ZIP1 = 'test data 1.zip';
+const ZIP2 = 'test data 2.zip';
 const DATA = resolve(__dirname, 'data');
 const TXT1_DATA = join(DATA, TXT1);
 const TXT2_DATA = join(DATA, TXT2);
-const ZIP_DATA = join(DATA, ZIP);
+const ZIP1_DATA = join(DATA, ZIP1);
+const ZIP2_DATA = join(DATA, ZIP2);
 const DIST_FOLDER = resolve(__dirname, 'dist');
 const UNZIPPED1 = join(DIST_FOLDER, TXT1);
 const UNZIPPED2 = join(DIST_FOLDER, TXT2);
-const ZIPPED = join(DIST_FOLDER, ZIP);
+const ZIPPED1 = join(DIST_FOLDER, ZIP1);
+const ZIPPED2 = join(DIST_FOLDER, ZIP2);
 
 describe('Test exports', () => {
   beforeEach(() => {
@@ -26,16 +29,19 @@ describe('Test exports', () => {
     expect(zipSync).toBeTruthy();
   });
 
-  test('zip "[test data 1.txt, test data 2.txt]" with zipSync', () => {
-    zipSync([TXT1_DATA, TXT2_DATA], ZIPPED);
-    unzipSync(ZIPPED, DIST_FOLDER);
+  test('zip "test data 1.txt" with zipSync', () => {
+    zipSync(TXT1_DATA, ZIPPED1);
 
-    let expected = readFileSync(TXT1_DATA);
-    let actual = readFileSync(UNZIPPED1);
+    const expected = readFileSync(ZIP1_DATA);
+    const actual = readFileSync(ZIPPED1);
     expect(actual).toEqual(expected);
+  });
 
-    expected = readFileSync(TXT2_DATA);
-    actual = readFileSync(UNZIPPED2);
+  test('zip "[test data 1.txt, test data 2.txt]" with zipSync', () => {
+    zipSync([TXT1_DATA, TXT2_DATA], ZIPPED2);
+
+    const expected = readFileSync(ZIP2_DATA);
+    const actual = readFileSync(ZIPPED2);
     expect(actual).toEqual(expected);
   });
 
@@ -44,7 +50,7 @@ describe('Test exports', () => {
   });
 
   test('unzip "test data.zip" with unzipSync', () => {
-    unzipSync(ZIP_DATA, DIST_FOLDER);
+    unzipSync(ZIP2_DATA, DIST_FOLDER);
 
     let expected = readFileSync(TXT1_DATA);
     let actual = readFileSync(UNZIPPED1);
