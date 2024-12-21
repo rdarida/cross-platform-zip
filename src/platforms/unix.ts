3c1cbcd0a35c68ab66b2d15a1d4cb1315791a3f1
Róbert Darida <rdarida@users.noreply.github.com>
2024-12-21 01:11:03 +0100

    refactor: remove for-of from unix.zipSync function

--- src/platforms/unix.ts
+++ src/platforms/unix.ts
@@ -20,16 +20,13 @@ export function zipSync(path: string | string[], dest: string): void {
     path = [path];
   }
 
-  for (const p of path) {
-    const options = {
-      cwd: dirname(p),
-      maxBuffer: Infinity,
-      silent: true,
-      windowsHide: true
-    };
+  const options = {
+    maxBuffer: Infinity,
+    silent: true,
+    windowsHide: true
+  };
 
-    exec(`zip -r -y "${dest}" "${basename(p)}"`, options);
-  }
+  exec(`zip -r -y "${dest}" ${path.map(p => `"${p}"`).join(' ')}`, options);
 }
 
 /**
