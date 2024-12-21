ef802753f55f5bdb798e6a947816d5d1419b3ecc
Róbert Darida <rdarida@users.noreply.github.com>
2024-12-21 14:10:33 +0100

    refactor: refactor unzip-cli.ts interface

--- src/unzip-cli.ts
+++ src/unzip-cli.ts
@@ -3,46 +3,42 @@ import yargs from 'yargs';
 
 import { unzipSync } from '.';
 
-(argv => {
-  try {
-    unzipSync(argv.path, argv.dest);
-  } catch (e: any) {
-    console.error(e);
-  }
-})(
-  yargs
-    .scriptName('cp-unzip')
-    .usage(
-      [
-        '$0 [options]',
-        'Extracts files from a specified archive (zipped) file.'
-      ].join('\n\n')
-    )
-    .help()
-    .options({
-      path: {
-        alias: 'p',
-        demandOption: true,
-        describe: 'Specifies the path to the archive file',
-        type: 'string'
-      },
-      dest: {
-        alias: 'd',
-        demandOption: true,
-        describe: 'Specifies the path to the output folder',
-        type: 'string'
-      }
-    })
-    .check(argv => {
-      if (Array.isArray(argv.path)) {
-        throw new Error('The "--path" parameter can only be specified once.');
-      }
+type Args = {
+  source: string;
+  destination: string;
+};
 
-      if (Array.isArray(argv.dest)) {
-        throw new Error('The "--dest" parameter can only be specified once.');
+yargs
+  .scriptName('cp-unzip')
+  .usage('$0 <source> <destination>')
+  .example(
+    '$0 source.zip destfolder',
+    'Extracts files from a specified archive (zipped) file.'
+  )
+  .help()
+  .command<Args>(
+    '$0 <source> <destination>',
+    'Extracts files from a specified archive (zipped) file.',
+    yargs => {
+      yargs
+        .positional('source', {
+          demandOption: true,
+          describe: 'Specifies the path to the archive file',
+          type: 'string'
+        })
+        .positional('destination', {
+          demandOption: true,
+          describe: 'Specifies the path to the output folder',
+          type: 'string'
+        });
+    },
+    args => {
+      try {
+        unzipSync(args.source, args.destination);
+      } catch (e: any) {
+        console.error(e);
       }
-
-      return true;
-    })
-    .parseSync()
-);
+    }
+  )
+  .strict()
+  .parseSync();
